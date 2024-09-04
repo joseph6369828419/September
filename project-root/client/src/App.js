e7b93ded648d77b34cf18cb0ea0b37138f19af2a
prithwin07 <prithwin2002@gmail.com>
2024-09-04 13:40:19 +0530

    success

---
+++ project-root/client/src/App.js
@@ -0,0 +1,49 @@
+// src/components/Register.js
+import React, { useState } from 'react';
+import axios from 'axios';
+
+function Register() {
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      await axios.post('/api/register', { username, password });
+      alert('User registered successfully');
+    } catch (error) {
+      console.error('There was an error registering the user!', error);
+    }
+  };
+
+  return (
+    <div>
+      <h2>Register</h2>
+      <form onSubmit={handleSubmit}>
+        <label>
+          Username:
+          <input
+            type="text"
+            value={username}
+            onChange={(e) => setUsername(e.target.value)}
+            required
+          />
+        </label>
+        <br />
+        <label>
+          Password:
+          <input
+            type="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+            required
+          />
+        </label>
+        <br />
+        <button type="submit">Register</button>
+      </form>
+    </div>
+  );
+}
+
+export default Register;
---
+++ project-root/server/server.js
@@ -0,0 +1,48 @@
+// server.js
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+const bodyParser = require('body-parser');
+const dotenv = require('dotenv');
+const app = express();
+const PORT = process.env.PORT || 3001;
+dotenv.config();
+// Middleware
+app.use(cors());
+app.use(bodyParser.json());
+const path = require('path');
+app.use(express.static(path.join(__dirname, 'client/build')));
+
+// Connect to MongoDB
+mongoose.connect(process.env.MONGODB_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+const userSchema = new mongoose.Schema({
+  username: { type: String, required: true, unique: true },
+  password: { type: String, required: true },
+});
+
+const User = mongoose.model('User', userSchema);
+
+// Register route
+app.post('/api/register', async (req, res) => {
+  const { username, password } = req.body;
+  try {
+    const newUser = new User({ username, password });
+    await newUser.save();
+    res.status(201).send('User registered successfully');
+  } catch (error) {
+    res.status(400).send('Error registering user');
+  }
+});
+
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
+});
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
