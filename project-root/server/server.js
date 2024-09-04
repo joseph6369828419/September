28a9cf4dc0cf9c22f249c768cecb7d9cbfa9e71b
prithwin07 <prithwin2002@gmail.com>
2024-09-04 14:31:25 +0530

    success

--- project-root/server/server.js
+++ project-root/server/server.js
@@ -39,6 +39,21 @@ app.post('/api/register', async (req, res) => {
 });
 
 
+app.get('/api/user/:username', async (req, res) => {
+  const { username } = req.params;
+  try {
+    const user = await User.findOne({ username });
+    if (user) {
+      res.status(200).json(user);
+    } else {
+      res.status(404).send('User not found');
+    }
+  } catch (error) {
+    res.status(500).send('Error retrieving user');
+  }
+});
+
+
 app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
 });
